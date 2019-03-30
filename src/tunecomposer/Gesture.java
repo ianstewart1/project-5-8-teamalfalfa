ab77e91158c5be6e1565fcdfffaf4ec155b48ef7
jamesbgd <jamesbgd@allennewell.whitman.edu>
2019-03-30 14:49:53 -0700

    Gesture file with some stuff

--- src/tunecomposer/Gesture.java
+++ src/tunecomposer/Gesture.java
@@ -5,10 +5,61 @@
  */
 package tunecomposer;
 
+import java.util.Set;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.shape.Rectangle;
+
 /**
  *
  * @author jamesbgd
  */
-public class Gesture {
+public class Gesture implements Playable{
+    
+    private boolean isSelected;
+    protected Rectangle boundingRect;
+    protected Set<Playable> elements;
+
+    public Gesture(){
+        isSelected = true;
+        //Calculate boundingRect and set elements = to currently selected
+    }
+    
+    @Override
+    public boolean getSelected() {
+        return isSelected;
+    }
+
+    @Override
+    public void setSelected(boolean selected) {
+        isSelected = selected;
+        if(selected){
+            boundingRect.getStyleClass().clear();
+            boundingRect.getStyleClass().add("selectedGesture");
+        }
+        else{
+            boundingRect.getStyleClass().clear();
+            boundingRect.getStyleClass().add("unselectedGesture");
+        }
+        elements.forEach((element) -> {
+            element.setSelected(selected);
+        });
+    }
+
+    @Override
+    public void setMovingCoords(MouseEvent event) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public void move(MouseEvent event) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void stopMoving(MouseEvent event) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+    
     
 }
